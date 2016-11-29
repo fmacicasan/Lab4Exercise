d54ba2517425617e093e861b746cae737bbb4afc
flo <flo.macicasan@gmail.com>
2016-11-29 20:05:58 +0200

    adds separate parity

--- src/ArrayProblems.java
+++ src/ArrayProblems.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * @author flo
  * @since 29/11/2016.
@@ -5,6 +7,35 @@
 public class ArrayProblems {
     public static void main(String[] args) {
 
+//        getMax();
+        separateParity();
+    }
+
+    private static void separateParity() {
+        int[] intArray = {9, 7, 14, 2, 5};
+        int[] evenArray = new int[intArray.length];
+        int[] oddArray = new int[intArray.length];
+        int indexOdd = 0, indexEven = 0;
+        for(int elem : intArray) {
+            if(elem % 2 == 0) {
+                evenArray[indexEven] = elem;
+                indexEven++;
+            } else {
+                oddArray[indexOdd] = elem;
+                indexOdd++;
+            }
+        }
+        System.out.println("Even");
+        for(int even : evenArray) {
+            System.out.println(even);
+        }
+        System.out.println("Odd");
+        for(int odd : oddArray) {
+            System.out.println(odd);
+        }
+    }
+
+    private static void getMax() {
         int[] intArray = {9, 7, 14, 2, 5};
         int max = intArray[0];
 
